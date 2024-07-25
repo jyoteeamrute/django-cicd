60546492ac60877fcefc61253329d5ac9e712201
sumit <sumit@gmail.com>
2024-07-25 08:52:43 -0400

    nginx conf

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -6,8 +6,9 @@ sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/django-cicd/nginx/nginx.conf /etc/nginx/sites-available/django-cicd
 sudo ln -s /etc/nginx/sites-available/django-cicd /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/django /etc/nginx/sites-enabled
-#sudo nginx -t
+
+
+sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
 
