f3e5aab0a492a53669ae50d1310f9ab64a1703d8
sumit <sumit@gmail.com>
2024-07-25 08:31:24 -0400

    env command

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
+# python3 -m venv /home/ubuntu/django-cicd/env
+# virtualenv /home/ubuntu/django-cicd/env
+# source /home/ubuntu/django-cicd/env/bin/activate
+# pip install -r /home/ubuntu/django-cicd/requirements.txt
 
-virtualenv /home/ubuntu/django-cicd/env
-source /home/ubuntu/django-cicd/env/bin/activate
+#!/bin/bash
+
+# Path to the virtual environment
+VENV_DIR="/home/ubuntu/django-cicd/env"
+
+# Create the virtual environment if it does not exist
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment at $VENV_DIR"
+    python3 -m venv $VENV_DIR
+fi
+
+# Activate the virtual environment
+source $VENV_DIR/bin/activate
+
+# Upgrade pip and install dependencies
+pip install --upgrade pip
 pip install -r /home/ubuntu/django-cicd/requirements.txt
