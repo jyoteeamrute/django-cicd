35add47bd8cc9b4a8be2d8793181c98855cd561c
sumit <sumit@gmail.com>
2024-07-26 02:46:57 -0400

    nginx conf

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -8,6 +8,10 @@ sudo cp /home/ubuntu/django-cicd/nginx/nginx.conf /etc/nginx/sites-available/dja
 # sudo ln -s /etc/nginx/sites-available/django-cicd /etc/nginx/sites-enabled/
 sudo rm /etc/nginx/sites-enabled/django-cicd
 sudo ln -s /etc/nginx/sites-available/django-cicd/etc/nginx/sites-enable
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl status nginx
+sudo systemctl reload nginx
 
 sudo nginx -t
 sudo gpasswd -a www-data ubuntu
