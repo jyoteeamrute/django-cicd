7410b4dc02af2f67f2343ae94f30ae8e35ec70bb
sumit <sumit@gmail.com>
2024-07-26 02:25:25 -0400

    conf

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -2,6 +2,15 @@
 sudo apt-get update
 sudo apt-get install python3-venv python3-pip
 
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/lib/dpkg/lock
+sudo rm /var/cache/apt/archives/lock
+sudo dpkg --configure -a
+
+sudo apt-get update
+sudo apt-get install python3-full
+sudo apt-get install pipx
+
 sudo apt install -y python3-pip
 sudo apt install -y nginx
 sudo apt install -y virtualenv
